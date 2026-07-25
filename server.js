cfde4d72bf7424dc6bb74e3ff5e1701bdfe62697
Eder-Rafael-Avila <ra52834094810@acaonsfatima.org.br>
2026-07-25 00:36:29 -0300

    autenticação completa
    busca por email, checagem de senha e redirecionamento para o dashboard feitos.

--- server.js
+++ server.js
@@ -1,14 +1,66 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mysql = require('mysql2');
+
+const conexao = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'admin_feira'
+})
+
+conexao.connect((erro) => {
+    if (erro) {
+        console.log('Houve um erro ao conectar no MySQL: ', erro);
+        return;
+    }
+
+    console.log('Sucesso ao conectar no MySQL!');
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.psot('/login', (req, res) => {
+app.post('/login', (req, res) => {
 
     const email = req.body.email;
     const password = req.body.password;
+
+    const sql = `
+        select * from administradores
+        where email = ?;
+    `
+
+    conexao.query(
+        sql,
+        [email],
+        (erro, resultado) => {
+
+            if (erro) {
+                console.log('Erro ao encontrar: ', erro);
+                return;
+            }
+            
+            if (resultado.length === 0) {
+                console.log('Nenhum usuário encontrado');
+                res.send('Nenhum usuário encontrado');
+                return;
+            }
+            
+            console.log(resultado);
+
+            const administrador = resultado[0];
+
+            if (password === administrador.senha) {
+                res.redirect('/dashboard.html');
+            } else {
+                console.log('Senha incorreta.');
+                res.send('Senha incorreta.');
+            }
+
+        }
+    )
 
 })
 
